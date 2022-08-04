9ecbc4e427a37b43dda11ba2c79797113ff606ca
MarkXtenda <mark.i2p.1998@gmail.com>
2022-08-04 18:01:54 +1000

    done

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,40 @@
-# code here!
+class School 
+    attr_reader :name
+    attr_accessor :roster
+
+    def initialize(name)
+        @name = name
+        @roster = Hash.new
+    end
+
+    def add_student(name, grade)
+        if (@roster.key?(grade))
+            @roster[grade] << name
+        else
+            @roster[grade] = []
+            @roster[grade] << name
+        end
+    end
+
+    def grade(grade)
+        @roster[grade] 
+    end
+
+    def sort()
+        new_hash = Hash.new
+        @roster.map do |key, value|
+            new_hash[key] = []
+            new_hash[key] = value.sort
+        end
+    new_hash
+    end
+end
+
+school = School.new("lil colledge")
+school.add_student("Kelly Kapowski", 10)
+school.add_student("Screech", 11)
+school.add_student("baby", 9)
+school.add_student("2baby", 9)
+school.add_student("AC Slater", 9)
+puts school.roster
+puts school.sort
